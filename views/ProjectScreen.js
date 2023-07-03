daa0feefc636b30c47db9b4f3247b7ad013dbc26
kelvin.rojas@tecsup.edu.pe <kelvin.rojas@tecsup.edu.pe>
2023-07-03 18:27:42 -0500

    header

--- views/ProjectScreen.js
+++ views/ProjectScreen.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-import { Calendar } from 'react-native-calendars';
+
 
 const ProjectScreen = () => {
   return (
     <View>
       <Text>Hola, este es el archivo ProjectScreen.js</Text>
-      <Calendar
-         
-      />
+      
     </View>
   );
 };
